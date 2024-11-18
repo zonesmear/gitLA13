1ba81210bd19c2541aaa44851f761353f709f790
Johnas Lopez <johnaslopez4@gmail.com>
2024-11-18 10:15:57 +0800

    Initial program

---
+++ program.py
@@ -0,0 +1,23 @@
+print("A - Add Record")
+print("B - View Records")
+print("C - Clear All Records")
+print("D - Exit")
+
+choice = ""
+
+while choice.upper() != 'D':
+    choice = input("ENTER SELECTION [A, B, C, or D]: ")
+
+    if choice.upper() == 'A':
+        print("Add Record")
+        addRec()
+    elif choice.upper() == 'B':
+        print("View Records")
+        viewRec()
+    elif choice.upper() == 'C':
+        print("Clear All Records")
+        clearRec()
+    elif choice.upper() == 'D':
+        print("Thank you!")
+    else:
+        print("Invalid selection! Please choose A, B, C, or D.")

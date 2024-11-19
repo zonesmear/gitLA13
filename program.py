ff3116b2e74c9f410ae5cb5e469c4a7eedaeae44
rheamariebacus <rheabacus2019@gmail.com>
2024-11-19 11:12:33 +0800

    test

--- program.py
+++ program.py
@@ -5,20 +5,4 @@ print("D - Exit")
 
 choice = ""
 
-while choice.upper() != 'D':
-    choice = input("ENTER SELECTION [A, B, C, or D]: ")
 
-    if choice.upper() == 'A':
-        print("Add Record")
-        addRec()
-    elif choice.upper() == 'B':
-        print("View Records")
-        viewRec()
-    elif choice.upper() == 'C':
-        print("Clear All Records")
-        clearRec()
-    elif choice.upper() == 'D':
-        print("Thank you!")
-    else:
-        print("Invalid selection! Please choose A, B, C, or D.")
-        
